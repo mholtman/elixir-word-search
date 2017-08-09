a20d2562be942d241bde8b493369ad31435b2318
Mark Holtman <mholtman@gmail.com>
2017-08-09 13:54:47 -0400

    Returns all coordinates of every occurence of a letter

--- lib/word_search.ex
+++ lib/word_search.ex
@@ -1,10 +1,25 @@
 defmodule WordSearch do
+  import :math, only: [sqrt: 1]
+
   def find_letter(list_of_letters, subject) do
-    index = List.flatten(list_of_letters) |>
-      Enum.find_index(fn(x) -> x == subject end)
-      size = Enum.count(list_of_letters)
-      y = rem(index, size)
-      x = Integer.floor_div(index, size)
-    {:ok, {x, y}}
+    flattened_list = List.flatten(list_of_letters)
+    # index = Enum.find_index(flattened_list, fn(x) -> x == subject end)
+
+    indexes = Enum.with_index(flattened_list)
+      |> Enum.filter_map(fn {x, _} -> x == subject end, fn {_, i} -> i end)
+
+    coordinates = indexes
+      |> Enum.map(fn(x) -> determine_coordinates(flattened_list, x) end)
+      |> Enum.reduce([], fn(x, acc) -> [ x | acc ] end)
+      |> Enum.reverse
+
+    {:ok, coordinates}
+  end
+
+  defp determine_coordinates(list_of_letters, index) do
+    number_of_rows = sqrt(Enum.count(list_of_letters)) |> trunc
+    y = rem(index, number_of_rows)
+    x = Integer.floor_div(index, number_of_rows)
+    {x, y}
   end
 end
--- test/word_search_test.exs
+++ test/word_search_test.exs
@@ -3,20 +3,21 @@ defmodule WordSearchTest do
   doctest WordSearch
 
   test "can determine if letter exists in row" do
-    assert WordSearch.find_letter(["a", "b", "c"], "a") == {:ok, {0, 0}}
-  end
-
-  test "can determine if letter exists later in a row" do
-    assert WordSearch.find_letter(["a", "b", "c"], "b") == {:ok, {0, 1}}
+    assert WordSearch.find_letter(["a"], "a") == {:ok, [{0, 0}]}
   end
 
   test "can find letter in two-dimensional array" do
     assert WordSearch.find_letter([["a", "b", "c"],
                                   ["d", "e", "f"],
-                                  ["g", "h", "i"]], "e") == {:ok, {1, 1}}
+                                  ["g", "h", "i"]], "e") == {:ok, [{1, 1}]}
   end
   test "can find letter in 2x2 two-dimensional array" do
     assert WordSearch.find_letter([["a", "b"],
-                                  ["c", "d"]], "d") == {:ok, {1, 1}}
+                                  ["c", "d"]], "d") == {:ok, [{1, 1}]}
+  end
+
+  test "can find multiple occurrences of a letter in a grid" do
+    assert WordSearch.find_letter([["a", "a"],
+                                  ["b", "c"]], "a") == {:ok, [{0, 0}, {0,1}]}
   end
 end
